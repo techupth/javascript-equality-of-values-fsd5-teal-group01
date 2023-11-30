1cc3b0aed1828073e8afd8ce11afecb70959089b
Kittipoom Puttaruksa <kittipoom@Kittipooms-MacBook-Pro.local>
2023-11-30 09:31:02 +0700

    ex:finish an equality-of-values-exercise

--- ex-1.js
+++ ex-1.js
@@ -7,13 +7,13 @@ Object.is("one", "two");
 Object.is({ name: "John", age: 30 }, { name: "John", age: 30 });
 Object.is(null, undefined);
 
-10 === 20;
-10 === "10"
-3.14 !== 3.14;
-"TechUp" !== "TechUp";
-NaN === NaN;
-0 !== -0;
+10 === 20; //false
+10 === "10"; //false
+3.14 !== 3.14; //false
+"TechUp" !== "TechUp"; //false
+NaN === NaN; //false
+0 !== -0; //false
 
 let objectA = { course: "Web Developer", price: 300000 };
 let objectB = { course: "Web Developer", price: 300000 };
-objectA === objectB;
+objectA === objectB; //false
